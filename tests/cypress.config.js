0ad5d42bc5559a6c2224bd59e7a791b64c493753
Emmanuel L <81372068+Mvnvs@users.noreply.github.com>
2025-01-26 23:20:42 +0100

    Update cypress.config.js

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000', // L'URL de base pour vos tests
     setupNodeEvents(on, config) {
-      // Vous pouvez ajouter des événements ou des plugins ici si nécessaire
-      return config;
     },
   },
 });
