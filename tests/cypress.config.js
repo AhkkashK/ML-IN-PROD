ecf8b9bb54accba3ce4ae538cbbaed35c6d621b1
Emmanuel L <81372068+Mvnvs@users.noreply.github.com>
2025-01-26 23:37:01 +0100

    Update cypress.config.js

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.js',
+    supportFile: 'cypress/support/e2e/test.cy.js',
     setupNodeEvents(on, config) {
       return config;
     },
